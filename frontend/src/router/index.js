c7f9b8448db4fabbe33ed1257ad44dca3b41ef65
jhpps1 <jungi2904@gmail.com>
2025-05-27 10:33:20 +0900

    1123

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,13 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CommunityView from '@/views/CommunityView.vue'
 import UserProfileView from '@/views/UserProfileView.vue'
-import TestBestseller from '@/components/test_bestseller.vue'
 
 const routes = [
   { path: '/', component: HomeView, name: 'Home' },
   { path: '/community', component: CommunityView, name: 'Community' },
   { path: '/profile', component: UserProfileView, name: 'UserProfile' },
-  { path: '/test-bestseller', component: TestBestseller, name: 'TestBestseller' },
 ]
 
 export default createRouter({
